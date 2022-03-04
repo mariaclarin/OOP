37a7f5d942c32830c83db305c52fe5b9735fc035
mariaclarin <maria.clarin@binus.ac.id>
2022-03-04 21:34:07 +0700

    create

---
+++ Practice1/Drill2.java
@@ -0,0 +1,6 @@
+public class no2{
+    public static void main(String[] args){
+        var NewLine = "\n";
+        System.out.println(NewLine);
+    }
+}
---
+++ Practice1/Drill4.java
@@ -0,0 +1,6 @@
+public class Drill1 {
+    public static void main(String[] args){
+        float x =2.5f, y=5.4f;
+        System.out.println(x); System.out.println(y);
+    }
+}
